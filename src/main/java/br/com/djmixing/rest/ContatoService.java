d8c2441df4c0cd4c38ca700b0303ff3119178478
Miquéias Pereira do Nascimento <miqueias.p.nasc@gmail.com>
2014-01-11 01:59:10 +0000

    init

---
+++ src/main/java/br/com/djmixing/rest/ContatoService.java
@@ -0,0 +1,18 @@
+package br.com.djmixing.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
+@Path("/contato")
+public class ContatoService {
+
+@GET
+@Path("/{param}")
+public Response getMsg(@PathParam("param") String msg) {
+  String output = "Hello " + msg;
+  return Response.status(200).entity(output).build();
+}
+
+}
