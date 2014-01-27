f57347b0005c8d6de4c3d2af70eada3205c545a7
afirmanet@gmail.com <>
2014-01-27 16:26:19 +0000

    bootstrap

--- src/main/java/br/com/djmixing/rest/ContatoService.java
+++ src/main/java/br/com/djmixing/rest/ContatoService.java
@@ -1,6 +1,7 @@
 package br.com.djmixing.rest;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
@@ -8,11 +9,13 @@ import javax.ws.rs.core.Response;
 @Path("/contato")
 public class ContatoService {
 
-@GET
-@Path("/{param}")
-public Response getMsg(@PathParam("param") String msg) {
-  String output = "Parâmetro recebido " + msg;
-  return Response.status(200).entity(output).build();
+    @GET
+    @Path("/{param}")
+    public Response getMsg(@PathParam("param") String msg) {
+
+        String output = "Parâmetro recebido " + msg;
+
+       return Response.status(200).entity(output).build();
 }
 
 }
